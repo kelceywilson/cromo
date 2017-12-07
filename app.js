01f384fa2b36ebb6a0ddfa2261c11f7fd5b56713
Kelcey Wilson <kelceywilson@gmail.com>
2017-12-06 16:18:26 -0800

    Add pg to for db testing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const csrf = require('csurf')
 
 const routes = require('./routes/routes.js')
 const logger = require('morgan')
+var pg = require('pg');
 
 const app = express()
 app.set('port', (process.env.PORT || 5000));
@@ -42,6 +43,18 @@ app.use(csrf()) // this needs to be above app.use(routes) - why?
 
 app.use(routes)
 app.use(logger('dev'))
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
